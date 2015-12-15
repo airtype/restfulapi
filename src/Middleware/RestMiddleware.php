c0d2575d762ccb01eefde319e0efd22833800079
Wes Rice <wes@airtype.com>
2015-12-14 21:09:35 -0500

    Refactored Rest middleware

--- src/Middleware/RestMiddleware.php
+++ src/Middleware/RestMiddleware.php
@@ -15,8 +15,8 @@ class RestMiddleware
      */
     public function __invoke(Request $request, Response $response, callable $next)
     {
-        // if ($request->getMethod() === 'GET' && !$request->getAttribute('elementType') && !$request->getAttribute('elementId') && !$request->getAttribute('action')) {
-        //     $permissions = \Craft\craft()->restfulApi_config->getCompiledElementsPermissions($is_authenticated);
+        // if ($method === 'GET' && !$request->getAttribute('elementType') && !$element_id && !$request->getAttribute('action')) {
+        //     $permissions = \Craft\\Craft\craft()->restfulApi_config->getCompiledElementsPermissions($is_authenticated);
 
         //     return $this->context->actionShowPermissions($permissions);
         // }
@@ -36,31 +36,122 @@ class RestMiddleware
             return $this->context->$method($this->variables);
         }
 
-        if ($request->getMethod() === 'GET' && !$request->getAttribute('elementId')) {
-            return $this->context->actionIndex($this->variables);
+        $action = $this->getAction($request->getMethod(), $request->getAttribute('elementId'));
+
+        return $this->$action($request, $response);
+    }
+
+    /**
+     * Get Action
+     *
+     * @param string  $method       Method
+     * @param string  $element_type Element Type
+     * @param boolean $element_id   Element Id
+     *
+     * @return [type]  [description]
+     */
+    private function getAction($method, $element_type, $element_id = null)
+    {
+        if ($method === 'GET' && !$element_id) {
+            return 'actionIndex';
         }
 
-        if ($request->getMethod() === 'GET' && $request->getAttribute('elementId')) {
-            return $this->context->actionShow($this->variables);
+        if ($method === 'GET' && $element_id) {
+            return 'actionShow';
         }
 
-        if ($request->getMethod() === 'POST' && !$request->getAttribute('elementId')) {
-            return $this->context->actionStore($this->variables);
+        if ($method === 'POST' && !$element_id) {
+            return 'actionStore';
         }
 
-        if (in_array($request->getMethod(), ['PUT', 'PATCH']) && $request->getAttribute('elementId')) {
-            return $this->context->actionUpdate($this->variables);
+        if (in_array($method, ['PUT', 'PATCH']) && $element_id) {
+            return 'actionUpdate';
         }
 
-        if ($request->getMethod() === 'DELETE' && $request->getAttribute('elementId')) {
-            return $this->context->actionDelete($this->variables);
+        if ($method === 'DELETE' && $element_id) {
+            return 'actionDelete';
         }
 
         $exception = new Exception();
         $exception
             ->setStatus(405)
-            ->setMessage(sprintf('`%s` method not allowed for resource `%s`.', $request->getMethod(), $request->getAttribute('elementType')));
+            ->setMessage(sprintf('`%s` method not allowed for resource `%s`.', $method, $request->getAttribute('elementType')));
 
         throw $exception;
+    }
+
+    /**
+     * Action Show
+     *
+     * @param array $variables Variables
+     *
+     * @return Response Response
+     */
+    private function actionIndex(Request $request, Response $response, array $variables = [])
+    {
+        $elements = \Craft\craft()->restfulApi->getElements($request);
+
+        return $response->setPaginatedCollection($elements);
+    }
+
+    /**
+     * Action Show
+     *
+     * @param array $variables Variables
+     *
+     * @return Response Response
+     */
+    private function actionShow(Request $request, Response $response, array $variables = [])
+    {
+        $element = \Craft\craft()->restfulApi->getElement($request);
+
+        return $response
+            ->setItem($element);
+    }
+
+    /**
+     * Action Store
+     *
+     * @param array $variables Variables
+     *
+     * @return Response Response
+     */
+    private function actionStore(Request $request, Response $response, array $variables = [])
+    {
+        $element = \Craft\craft()->restfulApi->saveElement($request);
+
+        return $response
+            ->setCreated()
+            ->setItem($element);
+    }
+
+    /**
+     * Action Update
+     *
+     * @param array $variables Variables
+     *
+     * @return Response Response
+     */
+    private function actionUpdate(Request $request, Response $response, array $variables = [])
+    {
+        $element = \Craft\craft()->restfulApi->saveElement($request);
+
+        return $response
+            ->setStatus(200, 'Element updated successfully.')
+            ->setItem($element);
+    }
+
+    /**
+     * Action Delete
+     *
+     * @param array $variables Variables
+     *
+     * @return Response Response
+     */
+    private function actionDelete(Request $request, Response $response, array $variables = [])
+    {
+        \Craft\craft()->restfulApi->deleteElement($request);
+
+        return $response->setStatus(204, 'Element deleted successfully.');
     }
 }
